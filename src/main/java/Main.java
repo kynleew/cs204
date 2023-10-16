5f3b96e40e43bbfd4df6a3dda2ad83690fc4b816
kynleew <kynlee.wil@gmail.com>
2023-10-16 16:28:09 -0600

    We love testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,83 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Testing Dockerfile");
     }
+
+    int add(int a , int b){
+        return a + b;
+    }
+
+    int subtract(int a , int b){
+        return a - b;
+    }
+
+    int multiply(int a , int b){
+        return a * b;
+    }
+
+    int divide(int a , int b){
+        return a / b;
+    }
+
+
+    /*
+    Returns the n'th number in the fibonacci sequence
+    https://en.wikipedia.org/wiki/Fibonacci_number
+    Example below
+    n = x
+    1 = 1
+    2 = 1
+    3 = 2
+    4 = 3
+    5 = 5
+    .
+    .
+    .
+    etc
+     */
+    int fibonacciNumberFinder(int n){
+        if (n <= 0) {
+            throw new IllegalArgumentException("Input must be a positive integer.");
+        }
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        int fib1 = 1;
+        int fib2 = 1;
+        int fibN = 0;
+
+        for (int i = 3; i <= n; i++) {
+            fibN = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = fibN;
+        }
+
+        return fibN;
+    }
+
+
+    /*
+    Returns binary value of the given int number
+    The binary number will be stored as a string
+    if int a = 0 then this method returns: 0
+    if int a = 10 then this method returns: 1010
+    if int a = 16 then this method returns: 10000
+     */
+    String intToBinaryNumber(int number){
+        if (number == 0) {
+            return "0";
+        }
+
+        StringBuilder binary = new StringBuilder();
+
+        while (number > 0) {
+            int remainder = number % 2;
+            binary.insert(0, remainder);
+            number /= 2;
+        }
+
+        return binary.toString();
+    }
+
 }
